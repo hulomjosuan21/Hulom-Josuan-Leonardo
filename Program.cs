4d728fd8002a72fc975b3aa7560853c5c2227abb
Josuan <hulomjosuan@gmail.com>
2024-03-08 10:39:53 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal1 = new Animal();
+            animal1.Name = "Dog";
+            animal1.Traits = "Friendly";
+            animal1.Teeth = "Sharp";
+            animal1.WhoAmI();
+        }
+    }
+
+    class Animal
+    {
+        public string? Name {get; set;}
+        public string? Traits {get; set;}
+        public string? Teeth {get; set;}
+
+        public void Cry()
+        {
+            Console.WriteLine("Woof!");
+        }
+
+        public void WhoAmI()
+        {
+            if(Name != null) Console.WriteLine("I am "+Name);
+
+            if(Traits != null) Console.WriteLine("My Traits are "+Traits);
+
+            if(Teeth != null) Console.WriteLine("I have a "+Teeth+" teeth");
+            
+    
+        }
+    }
+}
